b5db1651b2d0704ba4954a98b8d1162da5903700
maczh <maczh@qq.com>
2025-11-22 15:48:20 +0800

    main中的router位置调整

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import os
 from pathlib import Path
 from sqlalchemy import  text
 from sqlalchemy.orm import  Session, declarative_base
-# from router.router import v1
 
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
@@ -19,14 +18,15 @@ from pmf.middleware.postlog import LoggingMiddleware
 from common.vars import global_vars
 
 global_vars.app = PMFApp.App(config_file=os.path.join(os.path.dirname(os.path.abspath(__file__)),"kmp.yml"))
-from fastapi import APIRouter
-from controller.ums_store import router as ums_store_router
-from controller.pay_channel import router as pay_channel_router
-from controller.pay_order import router as pay_order_router
-router = APIRouter(prefix="/api/v1")
-router.include_router(ums_store_router)
-router.include_router(pay_channel_router)
-router.include_router(pay_order_router)
+# from fastapi import APIRouter
+# from controller.ums_store import router as ums_store_router
+# from controller.pay_channel import router as pay_channel_router
+# from controller.pay_order import router as pay_order_router
+# router = APIRouter(prefix="/api/v1")
+# router.include_router(ums_store_router)
+# router.include_router(pay_channel_router)
+# router.include_router(pay_order_router)
+from router.router import v1
 
 
 if __name__ == "__main__":
@@ -40,7 +40,7 @@ if __name__ == "__main__":
         allow_headers=["*"],
     )
     # myapp.app.include_router(router)
-    global_vars.app.app.include_router(router)
+    global_vars.app.app.include_router(v1)
     # PMFApp.app = myapp
     # global_vars.app = myapp
     global_vars.app.run()
